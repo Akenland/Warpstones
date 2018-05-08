098043d91d157a66fc3a1076449704aa74cb366f
Kyle Nanakdewa <KyleN@live.ca>
2018-05-08 15:54:17 -0400

    chance for multiple warp dust, random drop from lapis ore, unbreaking

--- src/com/kylenanakdewa/warpstones/items/ItemListener.java
+++ src/com/kylenanakdewa/warpstones/items/ItemListener.java
@@ -10,9 +10,13 @@ import com.kylenanakdewa.warpstones.Warpstone;
 import com.kylenanakdewa.warpstones.events.WarpstoneActivateEvent;
 import com.kylenanakdewa.warpstones.events.PlayerWarpEvent.WarpCause;
 
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
@@ -39,13 +43,28 @@ public final class ItemListener implements Listener {
 	}
 
 
-	@EventHandler(priority = EventPriority.MONITOR)
+	/**
+	 * Gives a random amount of Warp Dust to the specified player.
+	 */
+	public static void giveRandomWarpDust(Player player){
+		if(ThreadLocalRandom.current().nextInt(100) > 100-ConfigValues.warpDustChance){
+			int count = ThreadLocalRandom.current().nextInt(4);
+			ItemStack dust = new ItemStack(WarpItems.WARP_DUST);
+			dust.setAmount(count);
+			player.getInventory().addItem(dust);
+		}
+	}
+
+
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onWarpstoneActivation(WarpstoneActivateEvent event){
 		if(event.isCancelled()) return;
 
 		// If player is holding a warp shard, attempt to link it
 		ItemStack itemInHand = event.getPlayer().getInventory().getItemInMainHand();
 		if(ConfigValues.warpShardsUsable && WarpItems.isWarpShard(itemInHand)){
+			event.setCancelled(true);
+
 			// If shard is linked, take the shard and warp the player
 			if(WarpItems.isWarpShardLinked(itemInHand)){
 				Warpstone dest = WarpItems.getLinkedShardWarpstone(itemInHand);
@@ -57,7 +76,11 @@ public final class ItemListener implements Listener {
 				}
 
 				Utils.sendActionBar(event.getPlayer(), "Warping to shard's stored destination...");
-				itemInHand.setAmount(itemInHand.getAmount()-1);
+
+				// Take shard, if unbreaking chance is too low
+				if(itemInHand.getEnchantmentLevel(Enchantment.DURABILITY)<ThreadLocalRandom.current().nextInt(1,10)){
+					itemInHand.setAmount(itemInHand.getAmount()-1);
+				}
 				event.getPlayer().getEquipment().setItemInMainHand(itemInHand);
 
 				new WarpPlayer(event.getPlayer()).warp(dest, false, WarpCause.SHARD);
@@ -73,8 +96,16 @@ public final class ItemListener implements Listener {
 
 
 		// If this isn't the last warpstone they visited, get a random number to see if they get a warp dust
-		if(!event.isLastWarpstone() && !event.isHomeWarpstone() && !event.isSpawnWarpstone() && ThreadLocalRandom.current().nextInt(100) > 100-ConfigValues.warpDustChance){
-			event.getPlayer().getInventory().addItem(WarpItems.WARP_DUST);
+		if(!event.isLastWarpstone() && !event.isHomeWarpstone() && !event.isSpawnWarpstone()){
+			giveRandomWarpDust(event.getPlayer());
+		}
+	}
+
+	@EventHandler
+	public void onLapisOreBreak(BlockBreakEvent event){
+		// If player is breaking lapis ore, event drops items, and item used does not have silk touch
+		if(event.getBlock().getType().equals(Material.LAPIS_ORE) && event.isDropItems() && !event.getPlayer().getEquipment().getItemInMainHand().containsEnchantment(Enchantment.SILK_TOUCH)){
+			giveRandomWarpDust(event.getPlayer());
 		}
 	}
 
