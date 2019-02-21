c09189bc0f70753c49e36fb7ed572f136a45bb10
Kyle Nanakdewa <KyleN@live.ca>
2019-02-21 17:41:29 -0500

    Show location on death

--- src/com/kylenanakdewa/warpstones/EventListener.java
+++ src/com/kylenanakdewa/warpstones/EventListener.java
@@ -1,9 +1,11 @@
 package com.kylenanakdewa.warpstones;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
@@ -12,11 +14,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+import com.kylenanakdewa.core.characters.players.PlayerCharacter;
 import com.kylenanakdewa.core.common.CommonColors;
 import com.kylenanakdewa.core.common.Utils;
+import com.kylenanakdewa.core.common.prompts.Prompt;
 import com.kylenanakdewa.core.common.prompts.PromptActionEvent;
 import com.kylenanakdewa.warpstones.events.PlayerWarpEvent.WarpCause;
 
@@ -102,4 +107,41 @@ public final class EventListener implements Listener {
 		}
 	}
 
+
+	// Death handler
+	@EventHandler
+	public void showLocOnDeath(PlayerDeathEvent event){
+		PlayerCharacter character = PlayerCharacter.getCharacter(event.getEntity());
+
+		Location loc = event.getEntity().getLocation();
+		String locString = loc.getBlockX()+" "+loc.getBlockY()+" "+loc.getBlockZ();
+		
+		Warpstone nearestWarp = Warpstone.getNearest(loc, 100, false);
+		String nearestWarpString = "";
+		if(nearestWarp!=null){
+			nearestWarpString += " (Nearest Warpstone: ";
+			if(nearestWarp.getDisplayName()!=null) nearestWarpString += nearestWarp.getDisplayName()+ ", ";
+			nearestWarpString +=  Math.round(nearestWarp.getLocation().distance(loc)) + "m away)";
+		}
+
+		// Prompt shown to player
+		Prompt deathPrompt = new Prompt();
+		deathPrompt.addQuestion(CommonColors.INFO+"You died at "+locString+nearestWarpString);
+
+		if(event.getEntity().hasPermission("warpstones.death.teleport")){
+			deathPrompt.addAnswer("Teleport to death location", "command_tp "+locString);
+			if(nearestWarp!=null) deathPrompt.addAnswer("Warp to nearest warpstone", "command_warp to "+nearestWarp.getIdentifier());
+		}
+
+		deathPrompt.display(event.getEntity());
+
+		// Message shown to realm members
+		if(character.getRealm()!=null){
+			Collection<Player> realmMembers = character.getRealm().getOnlinePlayers();
+			realmMembers.remove(event.getEntity());
+			for(Player member : realmMembers){
+				member.sendMessage(character.getName()+CommonColors.INFO+" died at "+locString+nearestWarpString);
+			}
+		}
+	}
 }
