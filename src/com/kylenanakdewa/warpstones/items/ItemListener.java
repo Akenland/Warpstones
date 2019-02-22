4d1e5238d21b99d950ff367a09095aa4ba02bcc7
Kyle Nanakdewa <KyleN@live.ca>
2019-02-22 18:25:35 -0500

    Added Warp Hearts and Charged Shards

--- src/com/kylenanakdewa/warpstones/items/ItemListener.java
+++ src/com/kylenanakdewa/warpstones/items/ItemListener.java
@@ -1,5 +1,6 @@
 package com.kylenanakdewa.warpstones.items;
 
+import java.util.Map.Entry;
 import java.util.concurrent.ThreadLocalRandom;
 
 import com.kylenanakdewa.core.common.CommonColors;
@@ -26,33 +27,68 @@ import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
 
 public final class ItemListener implements Listener {
-    
-    //// Make sure that warp shards can only be crafted from warp dust
+
+	//// Make sure that warp items can only be crafted from the appropriate item
 	@EventHandler
-	public void onWarpShardCraft(CraftItemEvent event){
+	public void onWarpItemCraft(CraftItemEvent event) {
 		CraftingInventory inv = event.getInventory();
 		// If the item being crafted is a warp shard
-		if(inv.getResult()!=null && inv.getResult().isSimilar(WarpItems.WARP_SHARD)){
-			//Utils.notifyAdmins("A warp shard is being crafted.");
-
+		if (inv.getResult() != null && inv.getResult().isSimilar(WarpItems.WARP_SHARD)) {
 			// Make sure every item is a warp dust, if not, cancel the crafting
-			for(ItemStack item : inv.getMatrix()){
-				if(item!=null && !item.isSimilar(WarpItems.WARP_DUST)){
+			for (ItemStack item : inv.getMatrix()) {
+				if (item != null && !item.isSimilar(WarpItems.WARP_DUST)) {
 					inv.setResult(null);
 					event.setCancelled(true);
-					//Utils.notifyAdmins("Blocked warp shard crafting, incorrect items.");
 				}
 			}
 		}
-	}
+		// If the item being crafted is a warp heart
+		if (inv.getResult() != null && inv.getResult().isSimilar(WarpItems.WARP_HEART)) {
+			// Make sure every item is a warp dust, if not, cancel the crafting
+			if (inv.getMatrix().length != 9
+					|| (inv.getMatrix()[0] != null && !inv.getMatrix()[0].isSimilar(WarpItems.WARP_DUST))
+					|| (inv.getMatrix()[1] != null && !inv.getMatrix()[1].isSimilar(WarpItems.WARP_SHARD))
+					|| (inv.getMatrix()[2] != null && !inv.getMatrix()[2].isSimilar(WarpItems.WARP_DUST))
+					|| (inv.getMatrix()[3] != null && !inv.getMatrix()[3].isSimilar(WarpItems.WARP_SHARD))
+					|| (inv.getMatrix()[4] != null && !inv.getMatrix()[4].getType().equals(Material.EYE_OF_ENDER))
+					|| (inv.getMatrix()[5] != null && !inv.getMatrix()[5].isSimilar(WarpItems.WARP_SHARD))
+					|| (inv.getMatrix()[6] != null && !inv.getMatrix()[6].isSimilar(WarpItems.WARP_DUST))
+					|| (inv.getMatrix()[7] != null && !inv.getMatrix()[7].isSimilar(WarpItems.WARP_SHARD))
+					|| (inv.getMatrix()[8] != null && !inv.getMatrix()[8].isSimilar(WarpItems.WARP_DUST))) {
+				inv.setResult(null);
+				event.setCancelled(true);
+			}
+		}
+		// If the item being crafted is a charged warp shard
+		if (inv.getResult() != null && inv.getResult().isSimilar(WarpItems.CHARGED_WARP_SHARD)) {
+			// Make sure item is a warp heart, if not, cancel the crafting
+			for (ItemStack item : inv.getMatrix()) {
+				if (item != null && item.getType().equals(Material.INK_SACK) && !item.isSimilar(WarpItems.WARP_HEART)) {
+					inv.setResult(null);
+					event.setCancelled(true);
+				}
+				// Use warp heart count to determine result amount
+				else if (WarpItems.isWarpHeart(item)) {
+					ItemStack newResult = inv.getResult();
+					newResult.setAmount(WarpItems.getWarpHeartCount(item) / 25);
+					inv.setResult(newResult);
+				}
+			}
+			// If crafting was not cancelled, add the location
+			if (inv.getResult() != null && !event.isCancelled()) {
+				inv.setResult(
+						WarpItems.getLinkedChargedWarpShard(event.getWhoClicked().getLocation(), inv.getResult()));
+			}
 
+		}
+	}
 
 	/**
 	 * Gives a random amount of Warp Dust to the specified player.
 	 */
-	public static ItemStack getRandomWarpDust(){
-		if(ThreadLocalRandom.current().nextInt(100) > 100-ConfigValues.warpDustChance){
-			int count = ThreadLocalRandom.current().nextInt(9);
+	public static ItemStack getRandomWarpDust() {
+		if (ThreadLocalRandom.current().nextInt(100) > 100 - ConfigValues.warpDustChance) {
+			int count = ThreadLocalRandom.current().nextInt(5);
 			ItemStack dust = new ItemStack(WarpItems.WARP_DUST);
 			dust.setAmount(count);
 			return dust;
@@ -60,31 +96,37 @@ public final class ItemListener implements Listener {
 		return new ItemStack(Material.AIR);
 	}
 
-
 	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onWarpstoneActivation(WarpstoneActivateEvent event){
-		if(event.isCancelled()) return;
+	public void onWarpstoneActivation(WarpstoneActivateEvent event) {
+		if (event.isCancelled())
+			return;
 
 		// If player is holding a warp shard, attempt to link it
 		ItemStack itemInHand = event.getPlayer().getInventory().getItemInMainHand();
-		if(ConfigValues.warpShardsUsable && WarpItems.isWarpShard(itemInHand)){
+		if (ConfigValues.warpShardsUsable && WarpItems.isWarpShard(itemInHand)) {
 			event.setCancelled(true);
 
 			// If shard is linked, take the shard and warp the player
-			if(WarpItems.isWarpShardLinked(itemInHand)){
+			if (WarpItems.isWarpShardLinked(itemInHand)) {
 				Warpstone dest = WarpItems.getLinkedShardWarpstone(itemInHand);
 
 				// If the shard is linked to this warpstone, tell the player
-				if(dest.equals(event.getWarpstone())){
-					event.getPlayer().sendMessage(CommonColors.INFO+"Your warp shard is linked to this warpstone. Use it at any other warpstone to return here.");
+				if (dest.equals(event.getWarpstone())) {
+					event.getPlayer().sendMessage(CommonColors.INFO
+							+ "Your warp shard is linked to this warpstone. Use it at any other warpstone to return here.");
 					return;
 				}
 
 				Utils.sendActionBar(event.getPlayer(), "Warping to shard's stored destination...");
 
 				// Take shard, if unbreaking chance is too low
-				if((itemInHand.getEnchantmentLevel(Enchantment.DURABILITY)*2)<ThreadLocalRandom.current().nextInt(1,10)){
-					itemInHand.setAmount(itemInHand.getAmount()-1);
+				if ((itemInHand.getEnchantmentLevel(Enchantment.DURABILITY) * 2) < ThreadLocalRandom.current()
+						.nextInt(1, 10)) {
+					itemInHand.setAmount(itemInHand.getAmount() - 1);
+
+					// If shard breaks, drop some dust
+					if (itemInHand.getAmount() == 0)
+						event.getPlayer().getInventory().addItem(getRandomWarpDust());
 				}
 				event.getPlayer().getEquipment().setItemInMainHand(itemInHand);
 
@@ -94,15 +136,51 @@ public final class ItemListener implements Listener {
 
 			// Otherwise, attempt to link it
 			else {
-				event.getPlayer().getEquipment().setItemInMainHand(WarpItems.getLinkedWarpShard(event.getWarpstone(), itemInHand));
-				event.getPlayer().sendMessage(CommonColors.MESSAGE+"Your warp shard was linked to this warpstone. Use it at any other warpstone to return here.");
+				event.getPlayer().getEquipment()
+						.setItemInMainHand(WarpItems.getLinkedWarpShard(event.getWarpstone(), itemInHand));
+				event.getPlayer().sendMessage(CommonColors.MESSAGE
+						+ "Your warp shard was linked to this warpstone. Use it at any other warpstone to return here.");
+
+				// Give some warp dust
+				event.getPlayer().getInventory().addItem(getRandomWarpDust());
 			}
 		}
 
+		else if(ConfigValues.warpShardsUsable && WarpItems.isChargedWarpShard(itemInHand)){
+			event.setCancelled(true);
+
+			// If shard is linked, take the shard and warp the player
+			if(WarpItems.isChargedWarpShardLinked(itemInHand)){
+				Utils.sendActionBar(event.getPlayer(), "Warping to shard's stored destination...");
+
+				// Take shard, if unbreaking chance is too low
+				if ((itemInHand.getEnchantmentLevel(Enchantment.DURABILITY) * 2) < ThreadLocalRandom.current()
+						.nextInt(1, 10)) {
+					itemInHand.setAmount(itemInHand.getAmount() - 1);
+
+					// If shard breaks, drop some dust
+					if (itemInHand.getAmount() == 0)
+						event.getPlayer().getInventory().addItem(getRandomWarpDust());
+				}
+				event.getPlayer().getEquipment().setItemInMainHand(itemInHand);
+
+				new WarpPlayer(event.getPlayer()).teleport(WarpItems.getLinkedChargedShardLocation(itemInHand), false);
+				return;
+			}
+		}
 
 		// If this isn't the last warpstone they visited, get a random number to see if they get a warp dust
-		if(!event.isLastWarpstone() && !event.isHomeWarpstone() && !event.isSpawnWarpstone()){
-			event.getPlayer().getInventory().addItem(getRandomWarpDust());
+		else if (!event.isLastWarpstone() && !event.isHomeWarpstone() && !event.isSpawnWarpstone()) {
+			ItemStack dust = getRandomWarpDust();
+			dust.setAmount(dust.getAmount() * 2);
+			event.getPlayer().getInventory().addItem(dust);
+
+			// Increment warp heart, if they have any
+			for(Entry<Integer,? extends ItemStack> item : event.getPlayer().getInventory().all(Material.INK_SACK).entrySet()){
+				if(WarpItems.isWarpHeart(item.getValue())){
+					event.getPlayer().getInventory().setItem(item.getKey(), WarpItems.incrementWarpHeartCounter(item.getValue()));
+				}
+			}
 		}
 	}
 
@@ -111,6 +189,7 @@ public final class ItemListener implements Listener {
 		// If player is breaking lapis ore, event drops items, item used does not have silk touch, and player not in creative
 		if(event.getBlock().getType().equals(Material.LAPIS_ORE) && event.isDropItems() && !event.getPlayer().getEquipment().getItemInMainHand().containsEnchantment(Enchantment.SILK_TOUCH) && !event.getPlayer().getGameMode().equals(GameMode.CREATIVE)){
 			ItemStack dust = getRandomWarpDust();
+			dust.setAmount(dust.getAmount()*2);
 
 			// Fortune boost
 			if(event.getPlayer().getEquipment().getItemInMainHand().getEnchantmentLevel(Enchantment.LOOT_BONUS_BLOCKS) > (ThreadLocalRandom.current().nextInt(10)+1)){
--- src/com/kylenanakdewa/warpstones/items/WarpItems.java
+++ src/com/kylenanakdewa/warpstones/items/WarpItems.java
@@ -10,9 +10,12 @@ import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.DyeColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.World;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.material.Dye;
@@ -38,6 +41,12 @@ public final class WarpItems {
 
 	// Warp Shard
 	public static ItemStack WARP_SHARD = new ItemStack(getRenamedLapis("Warp Shard"));
+
+	// Warp Heart
+	public static ItemStack WARP_HEART = new ItemStack(getRenamedLapis("Warp Heart"));
+	
+	// Warp Heart
+	public static ItemStack CHARGED_WARP_SHARD = new ItemStack(getRenamedLapis("Charged Warp Shard"));
 
 
 	//// Warp Shards
@@ -92,14 +101,129 @@ public final class WarpItems {
 	}
 
 
+	//// Warp Hearts
+	// Check if an itemstack is a warp heart
+	public static boolean isWarpHeart(ItemStack item){
+		if(item==null || !item.getType().equals(Material.INK_SACK)) return false;
+		return item.getItemMeta().getDisplayName().equals(ChatColor.BLUE+"Warp Heart");
+	}
+
+	/** Get the count on a heart. */
+	public static int getWarpHeartCount(ItemStack warpHeart){
+		// Check if it's a warp shard
+		if(!isWarpHeart(warpHeart) || !warpHeart.getItemMeta().hasLore()) return 0;
+
+		// Get the percentage charge in the lore text.
+		return Integer.parseInt(ChatColor.stripColor(warpHeart.getItemMeta().getLore().get(0)).split("%")[0]);
+	}
+
+	/** Increment the counter on the heart. */
+	public static ItemStack incrementWarpHeartCounter(ItemStack warpHeart){
+		// Check if it's a warp shard
+		if(!isWarpHeart(warpHeart)) return warpHeart;
+
+		// Get the original count
+		int count = getWarpHeartCount(warpHeart) + 1;
+		if(count>100) return warpHeart;
+
+		// Set the new lore line
+		ItemMeta itemMeta = warpHeart.getItemMeta();
+		List<String> loreText = new ArrayList<String>(Arrays.asList(ChatColor.GRAY.toString()+count+"% charged"));
+		itemMeta.setLore(loreText);
+		warpHeart.setItemMeta(itemMeta);
+
+		return warpHeart;
+	}
+
+
+	//// Charged Warp Shards
+	// Get a Charged Warp Shard that is linked to a location. If an existing warp shard is specified, it is converted to a linked warp shard.
+	public static ItemStack getLinkedChargedWarpShard(Location location, ItemStack existingWarpShard){
+		// Get a normal warp shard
+		ItemStack linkedShard = existingWarpShard;
+
+		// If it's a warp shard, add the warpstone name in the lore, hidden with magic text (&k), and the X and Z co-ords
+		if(isWarpShard(linkedShard)){
+			String locString = location.getBlockX()+" "+location.getBlockY()+" "+location.getBlockZ();
+			ItemMeta itemMeta = linkedShard.getItemMeta();
+			List<String> loreText = new ArrayList<String>(Arrays.asList(
+				ChatColor.BLUE.toString()+ChatColor.MAGIC.toString()+location.getWorld()+" "+locString,
+				ChatColor.GRAY.toString()+locString
+				));
+			itemMeta.setLore(loreText);
+			linkedShard.setItemMeta(itemMeta);
+		}
+
+		return linkedShard;
+	}
+	public static ItemStack getLinkedChargedWarpShard(Location location){
+		return getLinkedChargedWarpShard(location, new ItemStack(CHARGED_WARP_SHARD));
+	}
+
+	// Check if an itemstack is a warp shard, linked or not
+	public static boolean isChargedWarpShard(ItemStack item){
+		if(item==null || !item.getType().equals(Material.INK_SACK)) return false;
+		return item.getItemMeta().getDisplayName().equals(ChatColor.BLUE+"Charged Warp Shard");
+	}
+
+	// Check if a shard is linked to a warpstone
+	public static boolean isChargedWarpShardLinked(ItemStack warpShard){
+		// Check if it's a warp shard
+		if(!isChargedWarpShard(warpShard)) return false;
+
+		// Check if it has lore text
+		return warpShard.getItemMeta().hasLore();
+	}
+
+	// Get the location that a charged warp shard is linked to
+	public static Location getLinkedChargedShardLocation(ItemStack warpShard){
+		// If item is not a linked warp shard, return null
+		if(!isChargedWarpShardLinked(warpShard)) return null;
+
+		// Get the lore text, and strip format codes to get the location
+		String[] coords = ChatColor.stripColor(warpShard.getItemMeta().getLore().get(0)).split(" ");
+		World world = Bukkit.getWorld(coords[0]);
+		if(world==null) return null;
+		Location loc = new Location(world, Integer.parseInt(coords[1]), Integer.parseInt(coords[2]), Integer.parseInt(coords[3]));
+		return loc;
+	}
+
+
 	//// Recipes
 	// Warp Shard
-	public static final ShapelessRecipe getWarpShardRecipe(){
+	public static final ShapedRecipe getWarpShardRecipe(){
 		// Prepare a shaped recipe with a key called warp_shard and a result of WARP_SHARD
-		ShapelessRecipe warpShardRecipe = new ShapelessRecipe(new NamespacedKey(WarpstonesPlugin.plugin, "warp_shard"), WARP_SHARD);
+		ShapedRecipe warpShardRecipe = new ShapedRecipe(new NamespacedKey(WarpstonesPlugin.plugin, "warp_shard"), WARP_SHARD);
 
-		// Set the ingredients to 2 warp dust
-		warpShardRecipe.addIngredient(2, new Dye(DyeColor.BLUE));
+		// Set the ingredients to 2x2 warp dust
+		warpShardRecipe.shape("DD","DD");
+		warpShardRecipe.setIngredient('D', new Dye(DyeColor.BLUE));
+
+		return warpShardRecipe;
+	}
+
+	// Warp Heart
+	public static final ShapedRecipe getWarpHeartRecipe(){
+		// Prepare a shaped recipe with a key called warp_heart and a result of WARP_HEART
+		ShapedRecipe warpHeartRecipe = new ShapedRecipe(new NamespacedKey(WarpstonesPlugin.plugin, "warp_heart"), WARP_HEART);
+
+		// Set the ingredients to 2x2 warp dust
+		warpHeartRecipe.shape("DSD","SES","DSD");
+		warpHeartRecipe.setIngredient('D', new Dye(DyeColor.BLUE));
+		warpHeartRecipe.setIngredient('S', new Dye(DyeColor.BLUE));
+		warpHeartRecipe.setIngredient('E', Material.EYE_OF_ENDER);
+
+		return warpHeartRecipe;
+	}
+
+	// Charged Warp Shard
+	public static final ShapelessRecipe getChargedWarpShardRecipe(){
+		// Prepare a shaped recipe with a key called warp_heart and a result of WARP_HEART
+		ShapelessRecipe warpShardRecipe = new ShapelessRecipe(new NamespacedKey(WarpstonesPlugin.plugin, "charged_warp_shard"), CHARGED_WARP_SHARD);
+
+		// Set the ingredients to warp heart + ender pearl
+		warpShardRecipe.addIngredient(new Dye(DyeColor.BLUE));
+		warpShardRecipe.addIngredient(Material.ENDER_PEARL);
 
 		return warpShardRecipe;
 	}
